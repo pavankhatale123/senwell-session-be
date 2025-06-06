ad5b63cd58d2294620a58efb916c2c5e7f8e6a78
manurag <manurag2144@gmail.com>
2025-06-06 22:18:32 +0530

    added timestamp fro session

--- models/sessions.js
+++ models/sessions.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const SessionSchema = new mongoose.Schema({
-  session: {
-    type: String,
-    required: true,
+const SessionSchema = new mongoose.Schema(
+  {
+    session: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    meetinglink: {
+      type: String,
+      required: true,
+    },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  meetinglink: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Session = mongoose.model("Session", SessionSchema);
 module.exports = Session;
